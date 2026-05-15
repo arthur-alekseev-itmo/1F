ebd51143d17e5270ed2d9b923f7172af5a41fb28
arthur100500 <arthur100500@gmail.com>
2026-05-15 15:29:25 +0300

    [fix] Fix mystical name
    Signed-off-by: arthur100500 <arthur100500@gmail.com>

--- test/test_interpreter.ml
+++ test/test_interpreter.ml
@@ -4,7 +4,7 @@ open OneF.ParserErrors
 
 let () =
   let input = In_channel.input_all stdin in
-  let parse_result = Parser.program_of_string input |> Result.get_ok' in
+  let parse_result = Parser.program_of_string input |> Result.get_ok in
   match parse_result with
   | Parser.Parsed (r, _) -> Interpreter.interpret r |> ignore
   | Parser.Failed (msg, range) -> ParserErrors.print input msg range
